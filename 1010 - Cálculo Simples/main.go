7f6945195ecaebc0f1bc1cbf4f73fba2e87ad0c1
Jhonata Souza <einasota@gmail.com>
2024-11-06 13:54:07 -0300

    submitting resolution 1010 and 1011

---
+++ 1010 - Cálculo Simples/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var codigo1, qtd1, codigo2, qtd2 int
+	var valor1, valor2 float64
+
+	fmt.Scanf("%d %d %f", &codigo1, &qtd1, &valor1)
+	fmt.Scanf("%d %d %f", &codigo2, &qtd2, &valor2)
+
+	total := float64(qtd1)*valor1 + float64(qtd2)*valor2
+	fmt.Printf("VALOR A PAGAR: R$ %.2f\n", total)
+}
---
+++ 1011 - Esfera/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var r float64
+	var pi float64 = 3.14159
+	fmt.Scanf("%f", &r)
+	calculo := (4/3.0) * pi * math.Pow(r,3)
+	fmt.Printf("VOLUME = %.3f\n", calculo)
+}
