b73566e2efbf0ee7a6dbe59f01ebefcab056ac2b
Jhonata Souza <einasota@gmail.com>
2025-02-01 21:53:03 -0300

    sending without resolution 2146

---
+++ 2146 - Senha/main.go
@@ -0,0 +1,16 @@
+// Link: https://resources.beecrowd.com/repository/UOJ_2146.html
+package main
+
+import "fmt"
+
+func main(){
+  var input int 
+  for true {
+    if input == int {
+      fmt.Scanf("%d", &input)
+      fmt.Println(input - 1)
+    } else {
+      break
+    }
+  }
+}
